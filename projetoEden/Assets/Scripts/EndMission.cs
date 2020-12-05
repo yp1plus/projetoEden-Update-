efe7c8821d6c5517666a01d0e266dc4b058c2f9c
Yure Pablo <yurepablo9@gmail.com>
2020-12-04 22:44:45 -0300

    UI from final mission done

--- projetoEden/Assets/Scripts/EndMission.cs
+++ projetoEden/Assets/Scripts/EndMission.cs
@@ -22,40 +22,9 @@ public class EndMission : MonoBehaviour
 
         if (player != null)
         {
-            //MainMenu.GoToFinal();
+            ;//MainMenu.GoToFinal();
         }
     }
 
 
 }
-
-/*public class Example : MonoBehaviour
-{
-    int quantGalinhas = 0;
-    bool chamaAcesa = false;
-    const char codigo = '!';
-    float alturaJogador = 42.1f;
-    string camera_A99 = "A99";
-    string camera_A98 = "A98";
-    string camera_A97 = "A97";
-
-    if (chamaAcesa == false)
-    {
-        //PararBloco();
-    }
-
-    //se ficar || correr
-
-    while (houverVacuo()){
-        //CriarBloco();
-    }
-
-    for (int i = 0; i < 7; i++){
-        for (int j = 0;  j< 12; j++){
-            //DestruirBloco(i,j);
-        }
-    }
-
-    //dar o resultado de quanto vai dar a senha depois de passar pelo código
-
-}*/
---
+++ projetoEden/Assets/Scripts/Examples.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Senha
+{
+    int quantGalinhas = 0;
+    bool chamaAcesa = false;
+    const char codigo = '!';
+    float alturaJogador = 42.1f;
+    string senha;
+
+    void CriarSenha()
+    {
+        if (codigo == '!' || alturaJogador > 50)
+        {
+            senha = "!987654321432120000;";
+        }
+        else if (chamaAcesa == false)
+        {
+            senha = "8654321432120000";
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (quantGalinhas > 0)
+            {
+                senha = senha + "false?";
+                quantGalinhas = quantGalinhas - 1;
+            }
+            else
+            {
+                senha = senha + "true?";
+                quantGalinhas = quantGalinhas + 1;
+            }
+        }
+
+        while (chamaAcesa){
+            senha = senha + "a";
+            chamaAcesa = false;
+        }
+
+        senha = senha + "0.123456789123456789123456789!";
+    }
+}
